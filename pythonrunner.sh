b48f55f2c4ab966fbde79aefd7941f2caf83cf2f
Anubhab Mukherjee <78078306+Anubhab-ai@users.noreply.github.com>
2022-07-06 22:33:29 +0530

    Update pythonrunner.sh

--- pythonrunner.sh
+++ pythonrunner.sh
@@ -8,7 +8,8 @@ fi
 return 0
 }
 com (){
-	python3 $a.py 
+	echo "___Your Output file___"
+	python3 $a.py
 	c=$?
 	if [ "$c" = "1" ]; then
 	echo "Plz fix the error and try again"
@@ -20,11 +21,30 @@ com (){
 		break
 	fi
 	
+	 
 }
+echo "\033[41;5m ℙ𝕐𝕋ℍ𝕆ℕ ℝ𝕌ℕℕ𝔼ℝ  \033[0m"
 echo "Enter the file name: "
 read a 
-touch $a.py
-echo "___Your Output file___"
 sf
-echo "___exiting___"
-echo "Hii from Anubhab"
+while :
+do
+echo " "
+echo "--------------------------"
+echo "1. I want to edit my code"
+echo "2.Exit"
+echo "--------------------------"
+echo " "
+read c
+case $c in
+	1 )
+		sf
+		;;
+	2 )
+		echo "\033[42;5m _____Exiting_____\033[0m"
+		echo "\033[44;5m@Anubhab\033[0m"
+		echo "\033[33mhttps://github.com/Anubhab-ai\033[0m"
+		exit 0
+		;;
+esac
+done
